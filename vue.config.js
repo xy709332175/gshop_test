3a35df9a8254034e64262d8ba9021d867b255471
MrXu <709332175@qq.com>
2019-11-08 14:21:21 +0800

    rem

---
+++ vue.config.js
@@ -0,0 +1,34 @@
+let path = require('path');
+const px2rem = require('postcss-px2rem')
+// 配置postcs-px2rem
+const postcss = px2rem({
+  remUnit: 37.5   //基准大小 baseSize，需要和rem.js中单位rem值占比一样相同
+})
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+    runtimeCompiler: true,
+    lintOnSave: false,
+    configureWebpack: {// 当前配置需要写入该选项中
+        resolve: {
+          extensions: ['.js', '.vue', '.json'],
+          alias: {
+            'vue$': 'vue/dist/vue.esm.js', 
+            '@': resolve('src'), // 设置文件查找路径，可简写文件查找路径
+            'components': resolve('src/components'),
+          }
+        }
+      },
+    css: { // 添加postcss配置
+      loaderOptions: {
+        postcss: {
+          plugins: [
+            postcss
+          ]
+        }
+      }
+    },
+  }
