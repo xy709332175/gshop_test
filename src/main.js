8e13358acbf227715bbdd94024b131b073eb57a0
MrXu <709332175@qq.com>
2019-11-08 20:09:44 +0800

    静态页面

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'lib-flexible/flexible'
+
+
 import router from './router'
+import GshopHeader from './components/GshopHeader/GshopHeader.vue'
 
-
+Vue.component('GshopHeader',GshopHeader)
 Vue.config.productionTip = false
 
 new Vue({
